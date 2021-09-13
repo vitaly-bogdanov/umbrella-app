ae04a8e167fc2f5e67f95e6adb736f49268dd1d0
Vitaly <kaliummati@gmail.com>
2021-09-13 17:16:30 +0900

    update

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
-
-import dotenv from 'dotenv';
-
 import { startMaster } from './root.master';
 import { startWorker } from './root.worker';
-
-dotenv.config({ path: `./.env.${process.env.NODE_ENV}` });
 
 startMaster();
 startWorker();
